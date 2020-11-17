5e75638ef23899b16441e7ca9d25f134d279f18f
kieuchicong99 <kieuchicong99@gmail.com>
2020-11-17 17:44:51 +0700

    update layout footer

--- client/src/layout/layout.js
+++ client/src/layout/layout.js
@@ -1,8 +1,21 @@
-import { SearchOutlined, HeartOutlined } from '@ant-design/icons';
+import {
+  SearchOutlined,
+  HeartOutlined,
+  FacebookFilled,
+  YoutubeFilled,
+  InstagramFilled,
+} from '@ant-design/icons';
 import { Button, Col, Layout, Row } from 'antd';
 import React from 'react';
 import { Route, Switch, BrowserRouter as Router, Link } from 'react-router-dom';
 
+import cash from '../assets/cash.svg';
+import installment from '../assets/installment.svg';
+import internet_banking from '../assets/internet-banking.svg';
+import jcb from '../assets/jcb.svg';
+import logo from '../assets/logo.webp';
+import mastercard from '../assets/mastercard.svg';
+import visa from '../assets/visa.svg';
 import bg from '../logo.svg';
 import Page1 from '../pages/Page1';
 import Page2 from '../pages/Page2';
@@ -36,7 +49,9 @@ function ContentLayout() {
           justifyContent: 'center',
           borderBottom: '1px solid  #f0f0f0',
         }}>
-        <Col span={8} />
+        <Col span={8}>
+          <img src={logo} style={{ width: '50px', heigh: '50px' }} />
+        </Col>
         <Col span={8}>
           <div style={{ display: 'flex', justifyContent: 'center' }}>
             <Link to="/thi-nghiem">Thí nghiệm </Link>
@@ -56,7 +71,7 @@ function ContentLayout() {
         </Col>
       </div>
 
-      <div style={{ paddingTop: '20px' }}>
+      <div style={{ padding: '30px 60px 50px' }}>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/thi-nghiem" component={Page1} />
@@ -110,11 +125,59 @@ function AppLayout() {
           <Content style={{ background: '#ffffff', padding: '10px' }}>
             <ContentLayout />
           </Content>
-          <Footer
-            style={{
-              textAlign: 'center',
-            }}>
-            Tìm phòng trọ | Group10 ©2020
+          <Footer>
+            <Row>
+              <Col span={6}>
+                <div className="title">hỗ trợ khách hàng</div>
+                <div>
+                  <Link to>
+                    <span style={{ color: 'rgb(196, 1, 26)', fontWeight: '500' }}>
+                      Hotline chăm sóc khách hàng : 1900-6035
+                    </span>
+                    <br /> (1000đ/phút, 8-21h kể cả T7,CN)
+                  </Link>
+                  <Link to> Chính sách bảo mật thanh toán</Link>
+                  <Link to> Điều khoản sử dụng</Link>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className="title">các chính sách </div>
+                <div>
+                  <Link to> Chính sách bảo mật thông tin cá nhân</Link>
+                  <Link to> Chính sách bảo mật thanh toán</Link>
+                  <Link to> Điều khoản sử dụng</Link>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className="title">hợp tác và liên kết</div>
+                <div>
+                  <Link to> Quy chế hoạt động </Link>
+                  <Link to> Đăng ký trở thành chủ trọ</Link>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className="title">kết nối với chúng tôi</div>
+
+                <FacebookFilled style={{ fontSize: 25, paddingRight: '20px' }} />
+                <YoutubeFilled style={{ fontSize: 25, paddingRight: '20px' }} />
+                <InstagramFilled style={{ fontSize: 25, paddingRight: '20px' }} />
+
+                <div className="title" style={{ marginTop: '20px' }}>
+                  phương thức thanh toán
+                </div>
+                <div className="payment">
+                  <img src={visa} />
+                  <img src={mastercard} />
+                  <img src={jcb} />
+
+                  <img src={cash} />
+                  <img src={internet_banking} />
+                  <img src={installment} />
+                </div>
+              </Col>
+            </Row>
+            <hr />
+            <div style={{ paddingTop: '20px' }}>Tìm phòng trọ | Group10 ©2020</div>
           </Footer>
         </Layout>
       </Router>
