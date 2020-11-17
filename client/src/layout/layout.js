c6c828c58828d995ca3fbb03f708c0b4e231e7a7
kieuchicong99 <kieuchicong99@gmail.com>
2020-11-17 17:45:36 +0700

    update layout footer

--- client/src/layout/layout.js
+++ client/src/layout/layout.js
@@ -83,102 +83,116 @@ function ContentLayout() {
   );
 }
 
+function AppHeader() {
+  return (
+    <Header
+      style={{
+        background: '#ffffff',
+        fontSize: '16px',
+        fontWeight: 'bolder',
+        textAlign: 'center',
+        padding: '10px 20px 0px 10px',
+        postion: 'sticky',
+        top: '0',
+      }}>
+      <Row>
+        <Col
+          span={8}
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-start',
+            fontFamily: 'Helvetica Neue',
+            color: '#999999',
+            fontSize: '14px',
+          }}>
+          <div style={{ paddingRight: '20px' }}>Chào mừng bạn đến với Tìm phòng trọ</div>
+          <div>Liên hệ với chúng tôi: 123-456-789</div>
+        </Col>
+        <Col span={8}>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </Col>
+        <Col span={8}>
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Button style={{ marginRight: '10px' }}> Đăng ký</Button>
+            <Button> Đăng nhập</Button>
+          </div>
+        </Col>
+      </Row>
+    </Header>
+  );
+}
+
+function AppFooter() {
+  return (
+    <Footer>
+      <Row>
+        <Col span={6}>
+          <div className="title">hỗ trợ khách hàng</div>
+          <div>
+            <Link to>
+              <span style={{ color: 'rgb(196, 1, 26)', fontWeight: '500' }}>
+                Hotline chăm sóc khách hàng : 1900-6035
+              </span>
+              <br /> (1000đ/phút, 8-21h kể cả T7,CN)
+            </Link>
+            <Link to> Chính sách bảo mật thanh toán</Link>
+            <Link to> Điều khoản sử dụng</Link>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div className="title">các chính sách </div>
+          <div>
+            <Link to> Chính sách bảo mật thông tin cá nhân</Link>
+            <Link to> Chính sách bảo mật thanh toán</Link>
+            <Link to> Điều khoản sử dụng</Link>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div className="title">hợp tác và liên kết</div>
+          <div>
+            <Link to> Quy chế hoạt động </Link>
+            <Link to> Đăng ký trở thành chủ trọ</Link>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div className="title">kết nối với chúng tôi</div>
+
+          <FacebookFilled style={{ fontSize: 25, paddingRight: '20px' }} />
+          <YoutubeFilled style={{ fontSize: 25, paddingRight: '20px' }} />
+          <InstagramFilled style={{ fontSize: 25, paddingRight: '20px' }} />
+
+          <div className="title" style={{ marginTop: '20px' }}>
+            phương thức thanh toán
+          </div>
+          <div className="payment">
+            <img src={visa} />
+            <img src={mastercard} />
+            <img src={jcb} />
+
+            <img src={cash} />
+            <img src={internet_banking} />
+            <img src={installment} />
+          </div>
+        </Col>
+      </Row>
+      <hr />
+      <div style={{ paddingTop: '20px' }}>Tìm phòng trọ | Group10 ©2020</div>
+    </Footer>
+  );
+}
+
 function AppLayout() {
   return (
     <div>
       <Router>
         <Layout style={{ minHeight: '100vh' }}>
-          <Header
-            style={{
-              background: '#ffffff',
-              fontSize: '16px',
-              fontWeight: 'bolder',
-              textAlign: 'center',
-              padding: '10px 20px 0px 10px',
-              postion: 'sticky',
-              top: '0',
-            }}>
-            <Row>
-              <Col
-                span={8}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'flex-start',
-                  fontFamily: 'Helvetica Neue',
-                  color: '#999999',
-                  fontSize: '14px',
-                }}>
-                <div style={{ paddingRight: '20px' }}>Chào mừng bạn đến với Tìm phòng trọ</div>
-                <div>Liên hệ với chúng tôi: 123-456-789</div>
-              </Col>
-              <Col span={8}>
-                <Link to="/">Home</Link> | <Link to="/about">About</Link>
-              </Col>
-              <Col span={8}>
-                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                  <Button style={{ marginRight: '10px' }}> Đăng ký</Button>
-                  <Button> Đăng nhập</Button>
-                </div>
-              </Col>
-            </Row>
-          </Header>
+          <AppHeader />
+
           <Content style={{ background: '#ffffff', padding: '10px' }}>
             <ContentLayout />
           </Content>
-          <Footer>
-            <Row>
-              <Col span={6}>
-                <div className="title">hỗ trợ khách hàng</div>
-                <div>
-                  <Link to>
-                    <span style={{ color: 'rgb(196, 1, 26)', fontWeight: '500' }}>
-                      Hotline chăm sóc khách hàng : 1900-6035
-                    </span>
-                    <br /> (1000đ/phút, 8-21h kể cả T7,CN)
-                  </Link>
-                  <Link to> Chính sách bảo mật thanh toán</Link>
-                  <Link to> Điều khoản sử dụng</Link>
-                </div>
-              </Col>
-              <Col span={6}>
-                <div className="title">các chính sách </div>
-                <div>
-                  <Link to> Chính sách bảo mật thông tin cá nhân</Link>
-                  <Link to> Chính sách bảo mật thanh toán</Link>
-                  <Link to> Điều khoản sử dụng</Link>
-                </div>
-              </Col>
-              <Col span={6}>
-                <div className="title">hợp tác và liên kết</div>
-                <div>
-                  <Link to> Quy chế hoạt động </Link>
-                  <Link to> Đăng ký trở thành chủ trọ</Link>
-                </div>
-              </Col>
-              <Col span={6}>
-                <div className="title">kết nối với chúng tôi</div>
 
-                <FacebookFilled style={{ fontSize: 25, paddingRight: '20px' }} />
-                <YoutubeFilled style={{ fontSize: 25, paddingRight: '20px' }} />
-                <InstagramFilled style={{ fontSize: 25, paddingRight: '20px' }} />
-
-                <div className="title" style={{ marginTop: '20px' }}>
-                  phương thức thanh toán
-                </div>
-                <div className="payment">
-                  <img src={visa} />
-                  <img src={mastercard} />
-                  <img src={jcb} />
-
-                  <img src={cash} />
-                  <img src={internet_banking} />
-                  <img src={installment} />
-                </div>
-              </Col>
-            </Row>
-            <hr />
-            <div style={{ paddingTop: '20px' }}>Tìm phòng trọ | Group10 ©2020</div>
-          </Footer>
+          <AppFooter />
         </Layout>
       </Router>
     </div>
