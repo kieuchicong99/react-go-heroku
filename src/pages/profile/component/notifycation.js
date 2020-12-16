f091a2a52d0291244ef55320b107145999b12dee
Hoang Long <truonghoanglong305@gmail.com>
2020-12-16 16:48:35 +0700

    Revert "Revert "insert profile""
    This reverts commit 7df403c8b658b9f56af4be83860cdcbb8f6f08e0.

---
+++ src/pages/profile/component/notifycation.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Title from "./title"
+export default class Notifycation extends Component {
+    render() {
+        return (
+            <div>
+                <Title name="THÔNG BÁO" />
+                <div style={{ marginTop: "10px" }}>
+                    Xem thông báo
+                </div>
+
+            </div>
+        )
+    }
+}
---
+++ src/pages/profile/component/statictis.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Title from "./title"
+export default class Statistic extends Component {
+    render() {
+        return (
+            <div>
+                <Title name="THỐNG KÊ" />
+                <div style={{ marginTop: "10px" }}>
+                    Xem thống kê
+                </div>
+
+            </div>
+        )
+    }
+}
