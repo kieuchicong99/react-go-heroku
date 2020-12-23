5545525aeca5881a1f2fd55a0ecf1a6e4dfccfcb
linhtunguyen <linhcva31399@gmail.com>
2020-12-23 09:12:38 +0700

    login,register

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,69 +1,115 @@
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { Form, Input, Button, Checkbox, Row, Col } from 'antd';
-import React from 'react';
+import { Form, Input, Button, Checkbox, Row, Col, notification } from 'antd';
+import React, { Component } from 'react';
+import { API_URLS } from '../../configs/api';
+import { apiCall } from '../../utilities/api';
 import './Login.scss';
 
-const Login = () => {
-  const onFinish = (values) => {
-    // eslint-disable-next-line no-console
-    console.log('Received values of form: ', values);
+
+class Login extends Component {
+  insertUser = async (payload, meta) => {
+   
+    const api = API_URLS.USER.login(payload);
+    const { response, error } = await apiCall({ ...api, payload });
+    if (!error && (response.status === 200 || response.status === 201)) {
+      if (meta && meta.onSuccess) {
+        meta.onSuccess();
+      }
+    } else if (meta && meta.onError) {
+      meta.onError(error);
+    }
+    return { response, error };
   };
 
-  return (
-    <div className="Login">
-      <div style={{ minWidth: '400px', minHeight: '400px' }}>
-        <Form
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}>
-          <Form.Item
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Username!',
-              },
-            ]}>
-            <Input prefix={<UserOutlined style={{ fontSize: '25px' }} />} placeholder="Username" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your Password!',
-              },
-            ]}>
-            <Input prefix={<LockOutlined style={{ fontSize: '25px' }} />} type="password" placeholder="Password" />
-          </Form.Item>
-          <Form.Item style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
+  OnClickUpdateSubmit = (value) => {
+
+    //console.log("form value: ", value)
+    this.insertUser(
+      value,
+      {
+
+        onSuccess: () => {
+          notification.open({
+            message: 'Success',
+            description: 'Login Success',
+            type: 'success',
+          });
+          console.log('successs')
+        
+        },
+        onError: (errorCode) => {
+          const res = JSON.parse(errorCode.responseText);
+
+
+          notification.open({
+            message: 'Error',
+            description: `${'Fail to signin'} - ${errorCode}`,
+            type: 'error',
+          });
+          console.log('erorr')
+        },
+      },
+    );
+   
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <div style={{ minWidth: '400px', minHeight: '400px' }}>
+          <Form
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={this.OnClickUpdateSubmit}>
+            <Form.Item
+              name="UserName"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Username!',
+                },
+              ]}>
+              <Input prefix={<UserOutlined style={{ fontSize: '25px' }} />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="PassWord"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Password!',
+                },
+              ]}>
+              <Input prefix={<LockOutlined style={{ fontSize: '25px' }} />} type="password" placeholder="Password" />
+            </Form.Item>
+            <Form.Item style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <a className="login-form-forgot" href="" style={{ color: 'blue', marginLeft: '1%' }}>
+                Forgot password
+            </a>
             </Form.Item>
 
-            <a className="login-form-forgot" href="" style={{ color: 'blue', marginLeft: '1%' }}>
-              Forgot password
-            </a>
-          </Form.Item>
-
-          <Form.Item>
-            <Row>
-              <Col span={8}>
-                <Button type="primary" htmlType="submit" className="login-form-button" style={{ alignItems: 'center' }}>
-                  Log in
+            <Form.Item>
+              <Row>
+                <Col span={8}>
+                  <Button type="primary" htmlType="submit" className="login-form-button" style={{ alignItems: 'center' }}>
+                    Log in
                 </Button>
-              </Col>
+                </Col>
 
-              <Col span={16}>
-                Or <a href="">register now!</a>
-              </Col>
-            </Row>
-          </Form.Item>
-        </Form>
+                <Col span={16}>
+                  Or <a href="">register now!</a>
+                </Col>
+              </Row>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Login;
