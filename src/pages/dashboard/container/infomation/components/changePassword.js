a2164c313f9f2f7ce6872cb37ac66cbce8ee0556
linhtunguyen <linhcva31399@gmail.com>
2020-12-23 15:42:42 +0700

    change password, info

--- src/pages/dashboard/container/infomation/components/changePassword.js
+++ src/pages/dashboard/container/infomation/components/changePassword.js
@@ -1,5 +1,8 @@
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button,notification } from 'antd';
 import React, { Component } from 'react';
+
+import { API_URLS } from './../../../../../configs/api';
+import { apiCall } from './../../../../../utilities/api';
 
 const layout = {
   labelCol: { span: 8 },
@@ -10,12 +13,55 @@ const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 export default class ChangePassword extends Component {
+
+  changePassWordUser = async (payload, meta) => {
+    const api = API_URLS.USER.changepassword(payload);
+    const { response, error } = await apiCall({ ...api, payload });
+    if (!error && (response.status === 200 || response.status === 201)) {
+      if (meta && meta.onSuccess) {
+        console.log('res:', response.data.Data);
+        if (response.data.Data != null && response.data.Data.length > 20) {
+          localStorage.setItem('motelFinderToken', response.data.Data);
+        }
+
+        meta.onSuccess();
+      }
+    } else if (meta && meta.onError) {
+      meta.onError(error);
+    }
+    return { response, error };
+  };
+
+
   onFinish = (values) => {
     // console.log('Success:', values);
+    this.changePassWordUser(values, {
+      onSuccess: () => {
+        notification.open({
+          message: 'Success',
+          description: 'Đổi mật khẩu thành công',
+          type: 'success',
+        });
+        setTimeout(() => {
+          window.location.href = '/login';
+        }, 2000);
+      },
+      onError: (errorCode) => {
+        notification.open({
+          message: 'Error',
+          description: `${'Đổi mật khẩu thất bại'}`,
+          type: 'error',
+        });
+        // console.log('erorr');
+      },
+    });
   };
 
   onFinishFailed = (errorInfo) => {
-    // console.log('Failed:', errorInfo);
+     console.log('Failed:', errorInfo);
+    setTimeout(() => {
+      window.location.href = '/changePassword';
+    }, 1000);
   };
 
   render() {
